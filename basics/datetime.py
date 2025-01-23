71bf905dc2e0a61913fdbc70e993e675a52151a8
Aiswariya Das <145064512+aiswariya13@users.noreply.github.com>
2025-01-23 15:53:19 +0530

    Initial commit

---
+++ basics/datetime.py
@@ -0,0 +1,8 @@
+dd=input("enter today's date: ")
+mm=input("enter the current month:")
+yy=input('enter current year: ')
+print("today's date is " ,yy,'-',mm,'-',dd)
+end='\n'
+hour=input('enter the time in hour ')
+minute=input('enter the minute')
+print("current time is ",hour,':',minute)
---
+++ basics/myname.py
@@ -0,0 +1,2 @@
+your_name=input('enter your name here: ')
+print('Hello,',your_name)
